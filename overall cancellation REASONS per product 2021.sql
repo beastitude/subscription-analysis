0d38b5555c1b00e6472fa2feab3955cc71b80b0d
beastitude <37851687+beastitude@users.noreply.github.com>
2021-12-06 14:19:33 -0500

    Delete overall cancellation REASONS per product 2021.sql

--- overall cancellation REASONS per product 2021.sql
+++
@@ -1,6 +0,0 @@
-SELECT  product_title,
-			cancellation_reason,
-			COUNT(cancellation_reason) AS amnt_cancellation_reason
-FROM Subscriptions
-WHERE STATUS = 'CANCELLED'
-GROUP BY product_title, cancellation_reason
