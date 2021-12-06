bdf92ddadb4b367ececc2bf3f17a973e1311ed81
beastitude <37851687+beastitude@users.noreply.github.com>
2021-12-06 14:19:17 -0500

    Delete most cancellation total summer.sql

--- most cancellation total summer.sql
+++
@@ -1,6 +0,0 @@
-SELECT  product_title,
-			COUNT(product_title) AS Subs_Cancelled
-FROM Subscriptions
-WHERE STATUS = 'CANCELLED' AND cancelled_at BETWEEN '2021-06-01' AND '2021-08-31'
-GROUP BY product_title
-ORDER BY Subs_Cancelled DESC
